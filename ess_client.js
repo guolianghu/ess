06d4f42261ad97d8e6f6541deb7bd5132a860bda
huguoliang <huguoliang@ehousechina.com>
2017-02-23 11:25:00 +0800

    test

--- ess_client.js
+++ ess_client.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var HttpRequest = require('./http_request');
 var AuthClient = require('./auth_client');
 var helper = require('./helper');
@@ -18,7 +20,7 @@ EssClient.prototype.GetObject = function (key) {
     var req = new HttpRequest(method, url_path_params, this.bucket, key);
     var client = new AuthClient(req, this.params);
 
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
         var callback = function (code, request) {
             if (code instanceof Error) {
                 return reject(code);
@@ -48,22 +50,37 @@ EssClient.prototype.GetAndSaveObject = function (key, file_path) {
     });
 };
 
-EssClient.prototype.PutObject = function (key, file_path, callback) {
+EssClient.prototype.PutObject = function (key, file_path) {
     var method = 'PUT';
     var url_path_params = '/' + key;
 
     var req = new HttpRequest(method, url_path_params, this.bucket, key, file_path);
     var client = new AuthClient(req, this.params);
 
-    if (this.private) {
-        helper.md5AsBase64(file_path, function (md5) {
-            req.setHeader("Content-MD5", md5);
-            req.setHeader("Date", helper.GetDate());
+    return new Promise( (resolve, reject) => {
+        var callback = function (code, request) {
+            if (code instanceof Error) {
+                return reject(code);
+            }
+
+            if (code !== 200) {
+                return reject(request);
+            }
+
+            resolve(request);
+        }
+
+        if (this.private) {
+            helper.md5AsBase64(file_path, function (md5) {
+                req.setHeader("Content-MD5", md5);
+                req.setHeader("Date", helper.GetDate());
+                client.SendRequest(callback);
+            });
+        } else {
             client.SendRequest(callback);
-        });
-    } else {
-        client.SendRequest(callback);
-    }
+        }
+    })
+
 };
 
 module.exports = EssClient;
